da91fdf8a2a2bea75c2c7fad7e887dd140c169e1
Jasdeep Saini <jsaini@outsitenetworks.com>
2018-12-01 13:44:49 -0600

    Day1 initial commit

---
+++ 2018/Day1/Day1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Day1
+//
+//  Created by Jasdeep Saini on 12/1/18.
+//  Copyright © 2018 Outsite Networks. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
